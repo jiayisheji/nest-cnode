843b5e412c0c9cb704e746870d01f3ca8fc76b17
jiayi <690405541@qq.com>
2018-08-23 18:19:43 +0800

    feat(main):  配置静态资源路由和加载静态资源中间件，设置模板引擎和路径

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+
+import { join } from 'path';
+import * as express from 'express';
+import * as loaderConnect from 'loader-connect';
+
 import { AppModule } from './app.module';
+import { Configuration } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // 注意：这个要在express.static之前调用，loader2.0之后要使用loader-connect
+  // 自动转换less为css
+  app.use(loaderConnect.less(__dirname));
+  // 所有的静态文件路径都前缀"/static", 需要使用“挂载”功能
+  app.use('/public', express.static(join(__dirname, '..', 'public')));
+  // 官方指定是这个 默认访问根目录
+  // app.useStaticAssets(join(__dirname, '..', 'public'));
+  // 配置模板（视图）的基本目录
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  // 配置视图引擎
+  app.setViewEngine('ejs');
+  await app.listen(Configuration.getEnv('PORT'));
 }
 bootstrap();
