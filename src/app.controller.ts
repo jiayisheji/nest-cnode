343f05c6aa925d2082482a677bb3ad13eb2e7e4c
jiayi <690405541@qq.com>
2018-08-29 12:52:33 +0800

    feat(views):  添加配置模板

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  root(): string {
-    return this.appService.root();
+  @Render('index')
+  root() {
+    return {};
   }
 }
