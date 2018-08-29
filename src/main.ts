abe0e2fe9a83e344153338dea0b019d06760bc31
jiayi <690405541@qq.com>
2018-08-29 12:54:39 +0800

    feat(main):  配置模板引擎和静态资源

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,30 @@ import { NestFactory } from '@nestjs/core';
 
 import { join } from 'path';
 import * as express from 'express';
+import * as ejsMate from 'ejs-mate';
 import * as loaderConnect from 'loader-connect';
-
 import { AppModule } from './app.module';
-import { Configuration } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const isDevelopment = true;
+  // 根目录 nest-cnode
+  const rootDir = join(__dirname, '..');
   // 注意：这个要在express.static之前调用，loader2.0之后要使用loader-connect
   // 自动转换less为css
-  app.use(loaderConnect.less(__dirname));
+  if (isDevelopment) {
+    app.use(loaderConnect.less(rootDir));
+  }
   // 所有的静态文件路径都前缀"/static", 需要使用“挂载”功能
-  app.use('/public', express.static(join(__dirname, '..', 'public')));
+  app.use('/public', express.static(join(rootDir, 'public')));
   // 官方指定是这个 默认访问根目录
   // app.useStaticAssets(join(__dirname, '..', 'public'));
+  // 指定视图引擎 处理.html后缀文件
+  app.engine('html', ejsMate);
+  // 视图引擎
+  app.set('view engine', 'html');
   // 配置模板（视图）的基本目录
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  // 配置视图引擎
-  app.setViewEngine('ejs');
-  await app.listen(Configuration.getEnv('PORT'));
+  app.setBaseViewsDir(join(rootDir, 'views'));
+  await app.listen(3000);
 }
 bootstrap();
